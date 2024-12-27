6a62f3afadebb691562611a2a9786dde8c388b0d
Oleh Maliuta <olehmaliutagithub@gmail.com>
2024-12-27 23:53:24 +0200

    added a page template for the authorized users and the app logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import LogInLayout from './layouts/LogInLayout/LogInLayout'
+import UserDisplayLayout from './layouts/UserDisplay/UserDisplayLayout'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={<UserDisplayLayout />}>
+          <Route index element={<></>} />
+        </Route>
         <Route path="/log-in" element={<LogInLayout />} />
       </Routes>
     </BrowserRouter>
